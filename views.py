2d033a8e8af7014d7d08f0fde08d56a702633dff
Aneesa-banu <70809852+Aneesa-banu@users.noreply.github.com>
2021-05-20 18:23:52 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,22 @@
+import json, shutil, requests
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+from pprint import pprint
+
+def get_attendence(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    pprint(text)    
+    return text
+
+#print(ocr_core('images/ocr_example_1.png'))
+
+
+
+   
